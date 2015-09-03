03f0b541b6fa2c9da30ff23939eb18e54a68687b
shilpa_sasidharan <Optimal@5678>
2015-09-03 19:53:22 -0400

    new changes
    R script

--- R/attributeimportanceJLI.R
+++ R/attributeimportanceJLI.R
@@ -7,24 +7,69 @@ computeAttributeImportance <- function(dbname1, user1, password1, host1, port1) 
   #con<-dbConnect(drv, dbname="JLI", user="postgres", password="postgres", host="localhost", port="5432")
   con<-dbConnect(drv, dbname=dbname1, user=user1, password=password1, host=host1, port=port1)
   
-  #need to read out the list of stores and other entities from DB
-  entities_query<-"select distinct store_number entity_id, 'store' entity_type from stores"
-  eq<-dbSendQuery(con, entities_query)
+  #read out the list of stores from DB
+  store_query<-"select distinct entity_group, entity, district, adi, store_type, store_number from eba_matrix_store"
+  sq<-dbSendQuery(con, store_query)
+  sq_matrix<-as.data.frame(fetch(sq, n=-1))
+  dbClearResult(sq)
+  
+  #read out the list of storetypes from DB
+  type_query<-"select distinct entity_group, entity, district, adi, store_type, store_number from eba_matrix_type"
+  tq<-dbSendQuery(con, type_query)
+  tq_matrix<-as.data.frame(fetch(tq, n=-1))
+  dbClearResult(tq)
+  
+  #read out the list of adi from DB
+  adi_query<-"select distinct entity_group, entity, district, adi, store_type, store_number from eba_matrix_adi"
+  aq<-dbSendQuery(con, adi_query)
+  aq_matrix<-as.data.frame(fetch(aq, n=-1))
+  dbClearResult(aq)
+  
+  #read out the list of district from DB
+  district_query<-"select distinct entity_group, entity, district, adi, store_type, store_number from eba_matrix_district"
+  dq<-dbSendQuery(con, district_query)
+  dq_matrix<-as.data.frame(fetch(dq, n=-1))
+  dbClearResult(dq)
+  
+  #read out the list of entity from DB
+  entity_query<-"select distinct entity_group, entity, district, adi, store_type, store_number from eba_matrix_entity"
+  eq<-dbSendQuery(con, entity_query)
   eq_matrix<-as.data.frame(fetch(eq, n=-1))
   dbClearResult(eq)
+  
+  #read out the list of district from DB
+  entity_group_query<-"select distinct entity_group, entity, district, adi, store_type, store_number from eba_matrix_entity_group"
+  egq<-dbSendQuery(con, entity_group_query)
+  egq_matrix<-as.data.frame(fetch(egq, n=-1))
+  dbClearResult(egq)
   
   #truncate the eba_results table before recalculating all importances
   truncate_query<-"truncate table eba_results"
   tq<-dbSendQuery(con, truncate_query)
   dbClearResult(tq)
   
-  computeImportance<-function(entity) {
+  computeImportance<-function(record) {
     tryCatch({ 
-      #entity<-c("123","store")
-      entity_id<-paste("'",entity[1],"'",sep="")
-      entity_type<-paste("'",entity[2],"'",sep="")
+      entity_group<-paste("'",record[1],"'",sep="")
+      entity<-paste("'",record[2],"'",sep="")
+      district<-paste("'",record[3],"'",sep="")
+      adi<-paste("'",record[4],"'",sep="")
+      store_type<-paste("'",record[5],"'",sep="")
+      store_number<-paste("'",record[6],"'",sep="")
       
-      tradeoff_query<-paste("select attr1, attr2, attr3, attr4, attr5, attr6, attr7, attr8 from eba_matrix where store_number=",entity_id,"order by attrs")
+      level<-"eba_matrix_store";
+      if (store_type != "'N/A'" && store_number == "'N/A'")
+        level<-"eba_matrix_type"
+      else if (adi !="'N/A'" && store_type == "'N/A'" && store_number == "'N/A'")
+        level<-"eba_matrix_adi"
+      else if (district !="'N/A'" && adi =="'N/A'" && store_type == "'N/A'" && store_number == "'N/A'")
+        level<-"eba_matrix_district"
+      else if (entity != "'N/A'" && district =="'N/A'" && adi =="'N/A'" && store_type == "'N/A'" && store_number == "'N/A'")
+        level<-"eba_matrix_entity"
+      else if (entity_group != "'N/A'" && entity == "'N/A'" && district =="'N/A'" && adi =="'N/A'" && store_type == "'N/A'" && store_number == "'N/A'")
+        level<-"eba_matrix_entity_group"
+      
+      tradeoff_query<-paste("select attr1, attr2, attr3, attr4, attr5, attr6, attr7, attr8 from",level,"where entity_group=",entity_group,"and entity=",entity,"and district=",district,"and adi=",adi,"and store_type=",store_type,"and store_number=",store_number,"order by attrs")
       
       tq<-dbSendQuery(con, tradeoff_query)
       eba_matrix<-as.data.frame(fetch(tq, n=-1))
@@ -34,22 +79,26 @@ computeAttributeImportance <- function(dbname1, user1, password1, host1, port1) 
       importance<-as.data.frame(uscale(btl)*100)
       names(importance)[1] <- "importance"
       
-      entity_ids<-c(entity[1], entity[1], entity[1], entity[1], entity[1], entity[1], entity[1], entity[1])
-      entity_types<-c(entity[2], entity[2], entity[2], entity[2], entity[2], entity[2], entity[2], entity[2])
+      entity_group_c<-c(record[1], record[1], record[1], record[1], record[1], record[1], record[1], record[1])
+      entity_c<-c(record[2], record[2], record[2], record[2], record[2], record[2], record[2], record[2])
+      district_c<-c(record[3], record[3], record[3], record[3], record[3], record[3], record[3], record[3])
+      adi_c<-c(record[4], record[4], record[4], record[4], record[4], record[4], record[4], record[4])
+      store_type_c<-c(record[5], record[5], record[5], record[5], record[5], record[5], record[5], record[5])
+      store_number_c<-c(record[6], record[6], record[6], record[6], record[6], record[6], record[6], record[6])
       attrs<-c("attr1","attr2","attr3","attr4","attr5","attr6","attr7","attr8")
       
-      table_prep<-as.data.frame(cbind(entity_ids, entity_types, attrs, importance))
+      table_prep<-as.data.frame(cbind(entity_group_c, entity_c, district_c, adi_c, store_type_c, store_number_c, attrs, importance))
       
       dbWriteTable(con, "eba_results", table_prep, row.names=F, append=T);
       
-      dbClearResult(tq)
+      dbClearResult(tq);
     },
     error = function(cond) {
       message("can't process")
     })
   }
   
-  apply(eq_matrix, 1, computeImportance)
+  apply(egq_matrix, 1, computeImportance)
   
   dbDisconnect(con)
 }
